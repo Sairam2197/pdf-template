e54cf5106d18a140b8b064ffdeb748996503c2f1
Sairam <mstrthedespicable@gmail.com>
2023-02-04 19:31:11 +0530

    added lines

--- main.py
+++ main.py
@@ -12,7 +12,13 @@ for index, row in csv_file.iterrows():
     pdf.set_font(family="Times", style="BI", size=24)
     pdf.set_text_color(200, 50, 75)
     pdf.cell(w=0, h=12, txt=row["Topic"], ln=1, align="L")
+
+    # adding lines
+    for i in range(20,298, 10):
+        pdf.line(10, i, 200, i)
     pdf.line(10, 21, 200, 21)
+
+
     """adding co-ordinates for underline - line under the text, since we have given the unit as mm
 we are giving the co-ordinates in mm pdf.line(x1, y1, x2,y2)"""
     #footer setting
@@ -21,11 +27,16 @@ we are giving the co-ordinates in mm pdf.line(x1, y1, x2,y2)"""
     pdf.set_text_color(200, 200, 75)
     pdf.cell(w=0, h=12, txt=row["Topic"], ln=1, align="R")
 
+
     for i in range(row["Pages"] - 1):
         pdf.add_page()
         pdf.ln(276)
         pdf.set_font(family="Times", style="I", size=10)
         pdf.set_text_color(200, 200, 75)
         pdf.cell(w=0, h=12, txt=row["Topic"], ln=1, align="R")
+
+        #adding lines
+        for i in range(20, 298, 10):
+            pdf.line(10, i, 200, i)
 
 pdf.output("output.pdf")
