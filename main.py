d136761460f6678eab713319c9c60c30a8997508
Sairam <mstrthedespicable@gmail.com>
2023-02-04 19:10:39 +0530

    added footers

--- main.py
+++ main.py
@@ -2,18 +2,30 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 csv_file = pd.read_csv("topics.csv")
 
 for index, row in csv_file.iterrows():
     pdf.add_page()
+    #setting Header
     pdf.set_font(family="Times", style="BI", size=24)
     pdf.set_text_color(200, 50, 75)
     pdf.cell(w=0, h=12, txt=row["Topic"], ln=1, align="L")
     pdf.line(10, 21, 200, 21)
     """adding co-ordinates for underline - line under the text, since we have given the unit as mm
 we are giving the co-ordinates in mm pdf.line(x1, y1, x2,y2)"""
+    #footer setting
+    pdf.ln(265)
+    pdf.set_font(family="Times", style="I", size=10)
+    pdf.set_text_color(200, 200, 75)
+    pdf.cell(w=0, h=12, txt=row["Topic"], ln=1, align="R")
+
     for i in range(row["Pages"] - 1):
         pdf.add_page()
+        pdf.ln(276)
+        pdf.set_font(family="Times", style="I", size=10)
+        pdf.set_text_color(200, 200, 75)
+        pdf.cell(w=0, h=12, txt=row["Topic"], ln=1, align="R")
 
 pdf.output("output.pdf")
